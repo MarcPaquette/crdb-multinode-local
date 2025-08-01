73b6c806a725828ed92aef977cb7f515afbd3d50
Marc Paquette <marc.paquette@cockroachlabs.com>
2025-08-01 11:07:33 -0400

    Add regions for local region testing

--- tmux_cluster.sh
+++ tmux_cluster.sh
@@ -10,6 +10,11 @@ BASE_HTTP_PORT="${BASE_HTTP_PORT:-8080}"
 STORE_DIRECTORY="${STORE_DIRECTORY:-temp_store}"
 TMUX_WINDOW="${TMUX_WINDOW:-crdb-cluster}"
 
+
+# define a comma-separated list of regions (no spaces!) and split into an array
+REGIONS_CSV="${REGIONS:-us-east,us-west,us-central}"
+IFS=',' read -r -a REGIONS <<< "$REGIONS_CSV"
+
 # Ensure we're inside a tmux session
 if [[ -z "${TMUX:-}" ]]; then
   echo "This script must be run inside an existing tmux session."
@@ -23,13 +28,14 @@ for i in $(seq 0 $((NODES-1))); do
 done
 join=${join%,}  # remove trailing comma
   
-
+region="${REGIONS[0]}"
 # Make the first pane the base (node 1)
 tmux new-window -n $TMUX_WINDOW cockroach start \
   --insecure \
   --store=$STORE_DIRECTORY/node1 \
   --listen-addr=localhost:$BASE_SQL_PORT \
   --http-addr=localhost:$BASE_HTTP_PORT \
+  --locality=region=$region \
   --join=$join
 
 # Add remaining nodes below
@@ -37,11 +43,17 @@ for i in $(seq 2 "$NODES"); do
   sql_port=$((BASE_SQL_PORT + i - 1))
   http_port=$((BASE_HTTP_PORT + i - 1))
   store="node$i"
+
+  # pick region by rotating through the array
+  idx=$(( (i-1) % ${#REGIONS[@]} ))
+  region="${REGIONS[$idx]}"
+
   tmux split-window -v cockroach start \
     --insecure \
     --store=$STORE_DIRECTORY/$store \
     --listen-addr=localhost:$sql_port \
     --http-addr=localhost:$http_port \
+    --locality=region=$region \
     --join=$join
   tmux select-layout -t "$TMUX_WINDOW" even-vertical
 done
